18e073fe757d787cd361c024cd77701031d9fb4a
auspy <vinayak.kshetez0102@gmail.com>
2022-10-27 13:00:01 +0530

    basic css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import './static/css/common.css'
+import './static/css/App.css';
 
 function App() {
   return (
     <div>
+      <h2>nice</h2>
+      <span className='regu12'>test</span>
     </div>
   );
 }
